db2848c79eba90eceb29ca33e606c0dfa09cb5e7
Remilia <HinanawiCirno@outlook.com>
2017-11-02 14:06:06 +0800

    init project

---
+++ src/main/java/me/mushen/examples/disruptor/Main.java
@@ -0,0 +1,13 @@
+package me.mushen.examples.disruptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-11-02
+ */
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+}
